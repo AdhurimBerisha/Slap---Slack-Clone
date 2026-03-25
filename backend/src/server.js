91f464ac58d16db02618cac4ae045076fd57005d
AdhurimBerisha <adhurimberisha07@gmail.com>
2026-03-26 00:11:22 +0100

    ENV

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,10 @@
 import express from "express";
+import { ENV } from "./config/env.js";
 
 const app = express();
-
-const PORT = process.env.PORT || 5001;
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(ENV.PORT, () => console.log(`Server started on port ${ENV.PORT}`));
