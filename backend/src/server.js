35916a65173b771a3331d4faf9d7a89730f714f4
AdhurimBerisha <adhurimberisha07@gmail.com>
2026-03-26 19:19:45 +0100

    Auth implement

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,35 @@
 import express from "express";
 import { ENV } from "./config/env.js";
 import { connectDb } from "./config/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import { functions, inngest } from "./config/inngest.js";
+import { serve } from "inngest/express";
 
 const app = express();
+
+app.use(express.json());
+app.use(clerkMiddleware());
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(ENV.PORT, () => {
-  console.log(`Server started on port ${ENV.PORT}`);
-  connectDb();
-});
+const startServer = async () => {
+  try {
+    await connectDb();
+    if (ENV.NODE_ENV !== "production") {
+      app.listen(ENV.PORT, () => {
+        console.log(`Server started on port ${ENV.PORT}`);
+        connectDb();
+      });
+    }
+  } catch (error) {
+    console.error("Error starting server", error);
+    process.exit(1);
+  }
+};
+
+startServer();
+export default app;
