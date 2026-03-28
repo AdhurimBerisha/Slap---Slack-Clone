4043bef82e89f30add82b21d99d920d6c92fe236
AdhurimBerisha <adhurimberisha07@gmail.com>
2026-03-28 15:36:37 +0100

    AuthProvider & Tools setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,23 @@
-import { StrictMode } from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { ClerkProvider } from "@clerk/react";
-import { BrowserRouter } from "react-router";
+import {
+  // Routes,
+  // Route,
+  BrowserRouter,
+  useLocation,
+  useNavigationType,
+  createRoutesFromChildren,
+  matchRoutes,
+} from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
+import AuthProvider from "./providers/AuthProvider.tsx";
+import * as Sentry from "@sentry/react";
+
+const queryClient = new QueryClient();
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -11,11 +25,30 @@ if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
 }
 
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [
+    Sentry.reactRouterV7BrowserTracingIntegration({
+      useEffect: React.useEffect,
+      useLocation,
+      useNavigationType,
+      createRoutesFromChildren,
+      matchRoutes,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <BrowserRouter>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+          <Toaster position="top-right" />
+        </QueryClientProvider>
       </BrowserRouter>
     </ClerkProvider>
   </StrictMode>,
---
+++ frontend/src/providers/AuthProvider.tsx
@@ -0,0 +1,45 @@
+import { createContext, useEffect, type ReactNode } from "react";
+import { useAuth } from "@clerk/react";
+import { axiosInstance } from "../lib/axios";
+import toast from "react-hot-toast";
+
+const AuthContext = createContext({});
+
+export default function AuthProvider({ children }: { children: ReactNode }) {
+  const { getToken } = useAuth();
+
+  useEffect(() => {
+    // setup axios interceptor
+
+    const interceptor = axiosInstance.interceptors.request.use(
+      async (config) => {
+        try {
+          const token = await getToken();
+          if (token) config.headers.Authorization = `Bearer ${token}`;
+        } catch (error: unknown) {
+          if (error instanceof Error) {
+            if (
+              error.message?.includes("auth") ||
+              error.message?.includes("token")
+            ) {
+              toast.error("Authentication issue. Pleased refresh the page");
+            }
+          }
+          console.log("Error getting token", error);
+        }
+        return config;
+      },
+      (error: unknown) => {
+        if (error instanceof Error) {
+          console.error("Axios request error", error);
+          return Promise.reject(error);
+        }
+      },
+    );
+
+    //   cleanup function to remove the interceptor, this is important to avoid memory leaks
+    return () => axiosInstance.interceptors.request.eject(interceptor);
+  }, [getToken]);
+
+  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+}
