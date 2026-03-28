79c66d8da7091427e09cb55d74fbac5e857c26f6
AdhurimBerisha <adhurimberisha07@gmail.com>
2026-03-28 01:55:02 +0100

    Auth Page Implemented

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import { UserButton } from "@clerk/react";
+
+type Props = {};
+const HomePage = (props: Props) => {
+  return (
+    <div>
+      <UserButton />
+      HomePage
+    </div>
+  );
+};
+export default HomePage;
